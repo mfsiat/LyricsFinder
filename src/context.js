2a2ddb00ead3418d87ee5b8a02bafe957bbaeb4f
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-13 02:23:53 +0600

    update

--- src/context.js
+++ src/context.js
@@ -1,1 +1,15 @@
 // Here lists the provider
+// because of exporting consumers also we are not exporting it as default
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  render() {
+    return (
+      <Context.Provider>
+        {this.props.children}
+      </Context.Provider>
+    )
+  }
+}
