25870d27a0a3aa4c3a6990ed35caf33857137065
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-13 18:38:47 +0600

    updated track and fetching the artist name from the api

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -4,9 +4,14 @@
 import React from 'react';
 
 const Track = (props) => {
+  const { track } = props;
   return (
-    <div>
-
+    <div className="col-md-6">
+      <div className="card mb-4 shadow-sm">
+        <div className="card-body">
+          <h5>{track.artist_name}</h5>
+        </div>
+      </div>
     </div>
   )
 }
