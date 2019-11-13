363722d282c227696b361d061f91af8aca66e728
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-13 21:47:16 +0600

    done with the lyrics page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
 
+import Lyrics from "./components/tracks/Lyrics";
+
 // Because The Provider is not a default we need to
 // import it inside curly braces 
 // import Provider from './context';
@@ -21,6 +23,7 @@ function App() {
         <div className="container">
           <Switch>
             <Route exact path="/" component={Index} />
+            <Route exact path="/lyrics/track/:id" component={Lyrics} />
           </Switch>
         </div>
       </React.Fragment>
--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,12 +1,69 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
+import Spinner from "../layout/Spinner";
 
 class Lyrics extends Component {
+  state = {
+    track: {},
+    lyrics: {}
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => {
+        this.setState({ lyrics: res.data.message.body.lyrics });
+
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then(res => {
+        this.setState({ track: res.data.message.body.track });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Lyrics</h1>
-      </div>
-    );
+    // we are destructuring the state and pulling out the data so that we dont need to call state to fetch track and lyrics data 
+    const { track, lyrics } = this.state; 
+    if(track === undefined || lyrics === undefined || Object.keys(track).length === 0 || Object.keys(lyrics).length === 0) {
+      return <Spinner />
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">Go Back</Link>
+          <div className="card">
+            <h5 className="card-header">
+      {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">
+                {lyrics.lyrics_body}
+              </p>
+            </div>
+          </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit Words</strong>:{' '}{track.explicit === 0 ? 'No' : 'Yes'}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>: {track.first_release_date}
+            </li>
+          </ul>
+        </React.Fragment>
+      )
+    }
   }
 }
 
