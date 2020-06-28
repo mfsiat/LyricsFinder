6967edbe5278b490d6827be3dfdab32f8edfc34d
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-06-29 01:27:23 +0600

    Updates

---
+++ precache-manifest.3d793b27df5ba80934cab089a4417e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bc054d77db9770ccde057291e6a010",
+    "url": "/LyricsFinder/index.html"
+  },
+  {
+    "revision": "c9c3943a0407d0d70b04",
+    "url": "/LyricsFinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "231b875a1a02ad7a03c3",
+    "url": "/LyricsFinder/static/js/2.5587540b.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/LyricsFinder/static/js/2.5587540b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c3943a0407d0d70b04",
+    "url": "/LyricsFinder/static/js/main.ce095ed9.chunk.js"
+  },
+  {
+    "revision": "e714c3f18ddc63635615",
+    "url": "/LyricsFinder/static/js/runtime-main.536e6266.js"
+  }
+]);
